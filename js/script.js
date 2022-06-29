633b25b040a1675abcd5ad30292e51edafeaa9b0
Rodion <sharoshkin1996@gmail.com>
2022-06-29 19:42:10 +0300

    add swipers

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const btn1 = document.querySelector('#btn1');
     const modal1 = new bootstrap.Modal(document.querySelector('#myModal1'));
-    btn1.addEventListener('click', function() {
+    btn1.addEventListener('click', function () {
         modal1.show();
     });
     const btn2 = document.querySelector('#btn2');
     const modal2 = new bootstrap.Modal(document.querySelector('#myModal2'));
-    btn2.addEventListener('click', function() {
+    btn2.addEventListener('click', function () {
         modal2.show();
     });
     const btn3 = document.querySelector('#btn3');
     const modal3 = new bootstrap.Modal(document.querySelector('#myModal3'));
-    btn3.addEventListener('click', function() {
-         modal3.show();
+    btn3.addEventListener('click', function () {
+        modal3.show();
     });
-    $("#myModal1, #myModal2, #myModal3").on('hidden.bs.modal', function(){
+    $("#myModal1, #myModal2, #myModal3").on('hidden.bs.modal', function () {
         alert("Спасибо, ожидайте звонка оператора");
     });
 
